73a122ef695320849bdf66f4b70fc583e4b2a671
20028951-maker <20028951@students.koi.edu.au>
2025-11-25 13:55:40 +1100

    Initial commit - Month1 Foundations repo

---
+++ EvenNumbersApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a number: ");
+        int n = int.Parse(Console.ReadLine());
+
+        PrintEvenNumbersUpTo(n);
+    }
+
+    static bool IsEven(int number)
+    {
+        return number % 2 == 0;
+    }
+
+    static void PrintEvenNumbersUpTo(int n)
+    {
+        for (int i = 0; i <= n; i++)
+        {
+            if (IsEven(i))
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
